871d0d20fb854efa8d9c5fdb2b2a48442cad057c
Víctor Fernández <victor.fernandez.gabriel@gmail.com>
2022-08-24 01:57:54 +0200

    feat: add babel for absolute import and update deps

---
+++ .babelrc.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+  presets: ['@babel/preset-env'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: './dist',
+      },
+    ],
+  ],
+}
